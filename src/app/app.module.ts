76455d2a55461c12c642c8c49ae3f848851c7fc9
Yume28 <steevensept09@gmail.com>
2023-03-29 21:10:22 +0800

    save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { ProductService } from './product.service';
 
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
-
-import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
@@ -15,8 +14,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
   { path: 'products', component: ProductListComponent },
-  { path: 'products/:id', component: ProductPageComponent },
-  { path: '',  redirectTo: 'products', pathMatch: 'full'}
+  { path: 'product/:id', component: ProductPageComponent },
+  { path: '', redirectTo: 'products', pathMatch: 'full' } 
 ];
 
 @NgModule({
